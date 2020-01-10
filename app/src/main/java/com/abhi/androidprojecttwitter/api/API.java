2d49e9971fe96000c4293319564e2a51c9225776
dukestabhi <new.abhishekh@gmail.com>
2020-01-10 13:38:03 +0545

    final twitter

---
+++ app/src/main/java/com/abhi/androidprojecttwitter/api/API.java
@@ -0,0 +1,45 @@
+package com.abhi.twitter.api;
+
+
+import com.abhi.twitter.model.Check;
+import com.abhi.twitter.model.ImageModel;
+import com.abhi.twitter.model.SignUpResponse;
+import com.abhi.twitter.model.TweetM;
+import com.abhi.twitter.model.User;
+import com.abhi.twitter.model.UserInfo;
+
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface API {
+    @FormUrlEncoded
+    @POST("users/login")
+    Call<SignUpResponse> checkUser(@Field("email") String username, @Field("password") String password);
+
+
+    @POST("users/signup")
+    Call<SignUpResponse> register(@Body User cud);
+
+    @Multipart
+    @POST("upload")
+    Call<ImageModel> uploadImage(@Part MultipartBody.Part imageFile);
+
+    @POST("users/check")
+    Call<Check> check(@Body User email);
+
+    @POST("users/showalltweet")
+    Call<List<TweetM>> GetTweet(@Header("Authorization") String token);
+
+    @POST("users/me")
+    Call<UserInfo> getUser(@Header("Authorization") String token);
+}
---
+++ app/src/main/java/com/abhi/androidprojecttwitter/model/Check.java
@@ -0,0 +1,13 @@
+package com.abhi.twitter.model;
+
+public class Check {
+ private    String status;
+
+    public Check(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
